15fe9dfa643012f440b293e0ca7ff39991148f4d
hechth <helge.hecht@recetox.muni.cz>
2022-02-18 14:16:02 +0000

    Moved extract_features function to separate file

--- R/unsupervised.R
+++ R/unsupervised.R
@@ -48,64 +48,6 @@ sort_samples_by_acquisition_number <- function (filenames) {
   sort(unlist(filenames))
 }
 
-extract_features <- function(
-  cluster,
-  filenames,
-  min_pres,
-  min_run,
-  mz_tol,
-  baseline_correct,
-  baseline_correct_noise_percentile,
-  intensity_weighted,
-  min_bandwidth,
-  max_bandwidth,
-  sd_cut,
-  sigma_ratio_lim,
-  shape_model,
-  peak_estim_method,
-  component_eliminate,
-  moment_power,
-  BIC_factor
-) {
-  clusterExport(cluster, list(
-    'proc.cdf',
-    'prof.to.features',
-    'load.lcms',
-    'adaptive.bin',
-    'find.turn.point',
-    'combine.seq.3',
-    'cont.index',
-    'interpol.area'
-  ))
-
-  parLapply(cluster, filenames, function(filename) {
-    profile <- proc.cdf(
-      filename = filename,
-      min.pres = min_pres,
-      min.run = min_run,
-      tol = mz_tol,
-      baseline.correct = baseline_correct,
-      baseline.correct.noise.percentile = baseline_correct_noise_percentile,
-      intensity.weighted = intensity_weighted,
-      do.plot = FALSE,
-      cache = FALSE
-    )
-    features <- prof.to.features(
-      a = profile,
-      min.bw = min_bandwidth,
-      max.bw = max_bandwidth,
-      sd.cut = sd_cut,
-      sigma.ratio.lim = sigma_ratio_lim,
-      shape.model = shape_model,
-      estim.method = peak_estim_method,
-      component.eliminate = component_eliminate,
-      power = moment_power,
-      BIC.factor = BIC_factor,
-      do.plot = FALSE
-    )
-  })
-}
-
 align_features <- function(sample_names, ...) {
   aligned <- feature.align(...)
   feature_names <- seq_len(nrow(aligned$pk.times))
