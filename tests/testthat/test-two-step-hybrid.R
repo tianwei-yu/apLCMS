c8eff77577759744c454c883e6a9ef3e1eecec01
maximskorik <maximskoryk@gmail.com>
2022-04-20 13:48:39 +0000

    remove redundant variables and comments from test

--- tests/testthat/test-two-step-hybrid.R
+++ tests/testthat/test-two-step-hybrid.R
@@ -11,20 +11,13 @@ test_that("basic two-step hybrid test", {
     file.copy(test_path, temp_path)
 
     expected_final_ftrs <- readRDS("../testdata/final_ftrs.Rda")
-    expected_all_ftrs <- readRDS("../testdata/all.detected.ftrs.Rda")
-    expected_batchwise <- readRDS("../testdata/batchwise.results.Rda")
     known_table <- arrow::read_parquet('../testdata/known_table.parquet')
-    info <- read.table("../testdata/two_step_hybrid_info.csv", sep=",")
 
-    # CRAN limits the number of cores available to packages to 2
-    # source https://stackoverflow.com/questions/50571325/r-cran-check-fail-when-using-parallel-functions#50571533
     chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
   
     if (nzchar(chk) && chk == "TRUE") {
-        # use 2 cores in CRAN/Travis/AppVeyor
         num_workers <- 2L
     } else {
-        # use all cores in devtools::test()
         num_workers <- parallel::detectCores()
     }
 
