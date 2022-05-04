fd80dd9dd5cfe07b19c8e038ad3aaf8b150db353
maximskorik <maximskoryk@gmail.com>
2022-05-04 10:05:34 +0000

    remove deprecated test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,21 +1,3 @@
-test_that("splitting files into batches", {
-    metadata <- read.table("../testdata/two_step_hybrid_info.csv",
-        sep = ",",
-        header = TRUE)
-    filenames <- c("../testdata/mbr_test0.mzml",
-        "../testdata/mbr_test1.mzml",
-        "../testdata/mbr_test2.mzml",
-        "../testdata/mbr_test0_copy.mzml")
-
-
-    actual <- split_files_into_batches(
-        filenames = filenames,
-        metadata = metadata)
-    expected <- tibble(filename = filenames, batch = c(1, 1, 2, 2))
-
-    expect_equal(actual, expected)
-})
-
 test_that("Utils: colnames pattern extraction", {
     dataframe <- dplyr::tibble(
         mz = numeric(),
