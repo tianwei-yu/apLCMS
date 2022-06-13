ffa99b11d2308eb31b529da803321b20d89e612d
hechth <helge.hecht@recetox.muni.cz>
2022-06-13 15:42:46 +0200

    Implemented test for "extract features" function

--- tests/testthat/test-extract_features.R
+++ tests/testthat/test-extract_features.R
@@ -1,44 +1,33 @@
-test_that("feature extraction test", {
-  ms_files <- c('../testdata/mbr_test0.mzml',
-                '../testdata/mbr_test1.mzml',
-                '../testdata/mbr_test2.mzml')
+test_that("extract single feature works", {
+  filename <- c("../testdata/mbr_test0.mzml")
 
-  extracted_feature_files <- c('../testdata/extracted_features/extracted_0.parquet',
-                               '../testdata/extracted_features/extracted_1.parquet',
-                               '../testdata/extracted_features/extracted_2.parquet')
+  extracted_features <- proc.cdf(filename, cache = FALSE)
+  actual <- prof.to.features(extracted_features, do.plot = FALSE)
 
-  expected <- lapply(extracted_feature_files, arrow::read_parquet)
-  expected <- lapply(expected, as.matrix)
+  expected <- as.data.frame(arrow::read_parquet("../testdata/extracted_features/extracted_0.parquet"))
+  actual <- as.data.frame(actual)
 
-  cluster = 4
+  actual <- unique(actual)
+  expected <- unique(expected)
 
-  if (!is(cluster, 'cluster')) {
-    cluster <- parallel::makeCluster(cluster)
-    on.exit(parallel::stopCluster(cluster))
-  }
+  keys <- c("mz", "pos", "sd1", "sd2")
 
-  # NOTE: side effect (doParallel has no functionality to clean up)
-  doParallel::registerDoParallel(cluster)
-
-  extracted <- extract_features(
-    cluster = cluster,
-    filenames = ms_files,
-    min_pres = 0.5,
-    min_run = 12,
-    mz_tol = 1e-05,
-    baseline_correct = 0,
-    baseline_correct_noise_percentile = 0.05,
-    intensity_weighted = FALSE,
-    min_bandwidth = NA,
-    max_bandwidth = NA,
-    sd_cut = c(0.01, 500),
-    sigma_ratio_lim = c(0.01, 100),
-    shape_model = "bi-Gaussian",
-    peak_estim_method = "moment",
-    component_eliminate = 0.01,
-    moment_power = 1,
-    BIC_factor = 2
+  actual <- dplyr::arrange_at(
+    actual, keys
+  )
+  expected <- dplyr::arrange_at(
+    expected, keys
   )
 
-  expect_equal(extracted, expected)
+  report <- dataCompareR::rCompare(actual, expected, keys = keys, roundDigits = 4, mismatches = 100000)
+  dataCompareR::saveReport(report, reportName = "extract_features_0", showInViewer = FALSE, HTMLReport = FALSE, mismatchCount = 10000)
+
+  expect_true(nrow(report$rowMatching$inboth) >= 0.9 * nrow(expected))
+
+  incommon <- as.numeric(rownames(report$rowMatching$inboth))
+
+  subset_actual <- actual %>% dplyr::slice(incommon)
+  subset_expected <- expected %>% dplyr::slice(incommon)
+
+  expect_equal(subset_actual$area, subset_expected$area, tolerance = 0.01 * max(expected$area))
 })
