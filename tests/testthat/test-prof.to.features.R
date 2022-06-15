2106f746f74d434fff8be2b00c69c1bd0064e067
hechth <helge.hecht@recetox.muni.cz>
2022-06-15 14:29:49 +0200

    Updated test cases for feature extraction

--- tests/testthat/test-prof.to.features.R
+++ tests/testthat/test-prof.to.features.R
@@ -1,11 +1,33 @@
-test_that("prof.to.features works", {
-    extracted_features <- readRDS('../testdata/proc_cdf_expected.Rds')
-    actual <- prof.to.features(
-        extracted_features,
-        sd.cut = c(0.1, 100),
-        sigma.ratio.lim = c(0.1, 10),
-        do.plot = FALSE)
+patrick::with_parameters_test_that(
+  "test prof.to.features",
+  {
+    testdata <- file.path("..", "testdata", .test_name)
+    input_path <- file.path(testdata, filename)
+    extracted_features <- readRDS(input_path)
 
-    expected <- readRDS("../testdata/prof_to_features_expected.Rds")
+    actual <- prof.to.features(
+      extracted_features,
+      sd.cut = sd_cut,
+      sigma.ratio.lim = sigma_ratio_lim,
+      do.plot = FALSE
+    )
+
+    expected_path <- file.path(testdata, expected_filename)
+    expected <- readRDS(expected_path)
     expect_equal(actual, expected)
-})
+  },
+  patrick::cases(
+    mbr_test0 = list(
+      filename = c("mbr_test0_cdf.Rds"),
+      expected_filename = "mbr_test0_features.Rds",
+      sd_cut = c(0.1, 100),
+      sigma_ratio_lim = c(0.1, 10)
+    ),
+    RCX_01_shortened_v2 = list(
+      filename = c("RCX_01_shortened_v2_cdf.Rds"),
+      expected_filename = "RCX_01_shortened_v2_features.Rds",
+      sd_cut = c(0.01, 500),
+      sigma_ratio_lim = c(0.01, 100)
+    )
+  )
+)
