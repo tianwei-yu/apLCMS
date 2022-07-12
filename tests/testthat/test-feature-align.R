760a9452507660d78c4eb35ec23bef675a4d8bf5
Matej Trojak <trojak@mail.muni.cz>
2022-07-12 14:26:04 +0200

    Update test files for align features step

--- tests/testthat/test-feature-align.R
+++ tests/testthat/test-feature-align.R
@@ -1,41 +1,46 @@
-test_that("feature align test", {
-  ms_files <- c('../testdata/mbr_test0.mzml',
-                '../testdata/mbr_test1.mzml',
-                '../testdata/mbr_test2.mzml')
-  files_list <- sort_samples_by_acquisition_number(ms_files)
-  sample_names <- get_sample_name(files_list)
+patrick::with_parameters_test_that(
+  "feature align test",
+  {
+    testdata <- file.path("..", "testdata")
+    
+    ms_files <- lapply(files, function(x) {
+      file.path(testdata, "input", paste0(x, ".mzML"))
+    })
+    
+    sample_names <- get_sample_name(unlist(ms_files))
+    
+    corrected_files <- lapply(files, function(x) {
+      file.path(testdata, "adjusted", paste0(x, ".parquet"))
+    })
+    corrected_features <- lapply(corrected_files, arrow::read_parquet)
+    corrected_features <- lapply(corrected_features, as.matrix)
+    
+    aligned_actual <- align_features(
+        sample_names = sample_names,
+        features = corrected_features,
+        min_occurrence = 2,
+        mz_tol_relative = NA,
+        rt_tol_relative = NA,
+        mz_max_diff = 10 * 1e-05,
+        mz_tol_absolute = 0.01,
+        do.plot = FALSE
+    )
+    
+    aligned_expected <- load_aligned_features(file.path(testdata, "aligned", 'rt_cross_table.parquet'),
+                                              file.path(testdata, "aligned", 'int_cross_table.parquet'),
+                                              file.path(testdata, "aligned", 'tolerances.parquet'))
   
-  corrected_files <- c('../testdata/feature-align/corrected_0.parquet',
-                       '../testdata/feature-align/corrected_1.parquet',
-                       '../testdata/feature-align/corrected_2.parquet')
-  
-  files_list <- sort_samples_by_acquisition_number(corrected_files)
-  corrected_features <- lapply(files_list, arrow::read_parquet)
-  corrected_features <- lapply(corrected_features, as.matrix)
-  
-  aligned_actual <- align_features(
-      sample_names = sample_names,
-      features = corrected_features,
+    expect_equal(aligned_actual, aligned_expected)
+  },
+  patrick::cases(
+    RCX_shortened = list(
+      files = c("RCX_06_shortened", "RCX_07_shortened", "RCX_08_shortened"),
       min_occurrence = 2,
       mz_tol_relative = NA,
       rt_tol_relative = NA,
       mz_max_diff = 10 * 1e-05,
       mz_tol_absolute = 0.01,
       do.plot = FALSE
+    )
   )
-  
-  rt_cross_table <- arrow::read_parquet('../testdata/feature-align/rt_cross_table.parquet')
-  int_cross_table <- arrow::read_parquet('../testdata/feature-align/int_cross_table.parquet')
-  tolerances_table <- arrow::read_parquet('../testdata/feature-align/tolerances.parquet')
-  
-  row.names(rt_cross_table) <- as.character(row.names(rt_cross_table))
-  row.names(int_cross_table) <- as.character(row.names(int_cross_table))
-  
-  aligned_expected <- list()
-  aligned_expected$mz_tolerance <- tolerances_table$mz_tolerance
-  aligned_expected$rt_tolerance <- tolerances_table$rt_tolerance
-  aligned_expected$rt_crosstab <- rt_cross_table
-  aligned_expected$int_crosstab <- int_cross_table
-
-  expect_equal(aligned_actual,aligned_expected)
-})
+)
