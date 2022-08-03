d762297f8603763a57f8910535efd169aac6139f
Zargham Ahmad <zargham.ahmad12@gmail.com>
2022-08-03 17:01:38 +0200

    reformat combine.seq.3

--- R/combine.seq.3.R
+++ R/combine.seq.3.R
@@ -14,18 +14,16 @@
 #' @export
 #' @examples
 #' combine.seq.3(retention_time_vector, masses, intensi)
-combine.seq.3 <-
-function(a, mz, inte)             ### the input need to be pre-ordered by a
-{
+combine.seq.3 <- function(a, mz, inte) {
     l <- length(a)
     breaks <- c(0, which(a[1:(l - 1)] != a[2:l]), l)
-    new.int <- new.mz <- rep(0, length(breaks)-1)
+    new.int <- new.mz <- rep(0, length(breaks) - 1)
     
     for (i in 1:(length(breaks) - 1)) {
-        this.int<-inte[(breaks[i] + 1):breaks[i + 1]]
-        this.mz<-mz[(breaks[i] + 1):breaks[i + 1]]
+        this.int <- inte[(breaks[i] + 1):breaks[i + 1]]
+        this.mz <- mz[(breaks[i] + 1):breaks[i + 1]]
         new.int[i] <- sum(this.int)
-        new.mz[i] <- median(this.mz[which(this.int==max(this.int))])
+        new.mz[i] <- median(this.mz[which(this.int == max(this.int))])
     }
     new.a <- unique(a)
     return(cbind(new.mz, new.a, new.int))
