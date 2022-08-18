ead81d9be911717a471bfe20e426197f9ece6b85
Zargham Ahmad <zargham.ahmad12@gmail.com>
2022-08-18 18:13:24 +0200

    updated full test case for extract features

--- tests/testthat/test-extract_features.R
+++ tests/testthat/test-extract_features.R
@@ -26,63 +26,53 @@ patrick::with_parameters_test_that(
       on.exit(parallel::stopCluster(cluster))
     }
 
-    if(!skip){
-      actual <- extract_features(
-        filenames,
-        cluster = cluster,
-        min_pres = min_pres,
-        min_run = min_run,
-        mz_tol = tol,
-        intensity_weighted = intensity_weighted,
-        sd_cut = sd_cut,
-        sigma_ratio_lim = sigma_ratio_lim,
-        baseline_correct = 0,
-        baseline_correct_noise_percentile = 0.05,
-        min_bandwidth = NA,
-        max_bandwidth = NA,
-        moment_power = 1,
-        BIC_factor = 2.0,
-        component_eliminate = 0.01,
-        peak_estim_method = "moment",
-        shape_model = "bi-Gaussian"
-      )
+    if(skip){
+      skip("skipping whole data test case")
+    }
 
-      expected_filenames <- lapply(files, function(x) {
-        file.path(testdata, "extracted", paste0(x, ".parquet"))
-      })
-
-      expected <- lapply(expected_filenames, arrow::read_parquet)
-      expected <- lapply(expected, as.data.frame)
-
-      actual <- unique(actual)
-      expected <- unique(expected)
-
-      keys <- c("mz", "pos", "sd1", "sd2")
-
-      actual <- lapply(actual, function(x) {
-        as.data.frame(x) |> dplyr::arrange_at(keys)
-      })
-      expected <- lapply(expected, function(x) {
-        as.data.frame(x) |> dplyr::arrange_at(keys)
-      })
-
-
-      for (i in seq_along(files)) {
-        actual_i <- actual[[i]]
-        expected_i <- expected[[i]]
-
-        report <- dataCompareR::rCompare(actual_i, expected_i, keys = keys, roundDigits = 4, mismatches = 100000)
-        dataCompareR::saveReport(report, reportName = files[[i]], showInViewer = FALSE, HTMLReport = FALSE, mismatchCount = 10000)
-
-        expect_true(nrow(report$rowMatching$inboth) >= 0.9 * nrow(expected_i))
-
-        incommon <- as.numeric(rownames(report$rowMatching$inboth))
-
-        subset_actual <- actual_i %>% dplyr::slice(incommon)
-        subset_expected <- expected_i %>% dplyr::slice(incommon)
-
-        expect_equal(subset_actual$area, subset_expected$area, tolerance = 0.01 * max(subset_expected$area))
-      }
+    actual <- extract_features(
+      filenames,
+      cluster = cluster,
+      min_pres = min_pres,
+      min_run = min_run,
+      mz_tol = tol,
+      intensity_weighted = intensity_weighted,
+      sd_cut = sd_cut,
+      sigma_ratio_lim = sigma_ratio_lim,
+      baseline_correct = 0,
+      baseline_correct_noise_percentile = 0.05,
+      min_bandwidth = NA,
+      max_bandwidth = NA,
+      moment_power = 1,
+      BIC_factor = 2.0,
+      component_eliminate = 0.01,
+      peak_estim_method = "moment",
+      shape_model = "bi-Gaussian"
+    )
+    expected_filenames <- lapply(files, function(x) {
+      file.path(testdata, "extracted", paste0(x, ".parquet"))
+    })
+    expected <- lapply(expected_filenames, arrow::read_parquet)
+    expected <- lapply(expected, as.data.frame)
+    actual <- unique(actual)
+    expected <- unique(expected)
+    keys <- c("mz", "pos", "sd1", "sd2")
+    actual <- lapply(actual, function(x) {
+      as.data.frame(x) |> dplyr::arrange_at(keys)
+    })
+    expected <- lapply(expected, function(x) {
+      as.data.frame(x) |> dplyr::arrange_at(keys)
+    })
+    for (i in seq_along(files)) {
+      actual_i <- actual[[i]]
+      expected_i <- expected[[i]]
+      report <- dataCompareR::rCompare(actual_i, expected_i, keys = keys, roundDigits = 4, mismatches = 100000)
+      dataCompareR::saveReport(report, reportName = files[[i]], showInViewer = FALSE, HTMLReport = FALSE, mismatchCount = 10000)
+      expect_true(nrow(report$rowMatching$inboth) >= 0.9 * nrow(expected_i))
+      incommon <- as.numeric(rownames(report$rowMatching$inboth))
+      subset_actual <- actual_i %>% dplyr::slice(incommon)
+      subset_expected <- expected_i %>% dplyr::slice(incommon)
+      expect_equal(subset_actual$area, subset_expected$area, tolerance = 0.01 * max(subset_expected$area))
     }
   },
   patrick::cases(
