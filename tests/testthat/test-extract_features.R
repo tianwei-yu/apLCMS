b84206b846d4d152a68bdb08f2bc5c3490e5f8d2
Zargham Ahmad <zargham.ahmad12@gmail.com>
2022-08-18 17:36:54 +0200

    added full test case for extract features

--- tests/testthat/test-extract_features.R
+++ tests/testthat/test-extract_features.R
@@ -26,61 +26,63 @@ patrick::with_parameters_test_that(
       on.exit(parallel::stopCluster(cluster))
     }
 
-    actual <- extract_features(
-      filenames,
-      cluster = cluster,
-      min_pres = min_pres,
-      min_run = min_run,
-      mz_tol = tol,
-      intensity_weighted = intensity_weighted,
-      sd_cut = sd_cut,
-      sigma_ratio_lim = sigma_ratio_lim,
-      baseline_correct = 0,
-      baseline_correct_noise_percentile = 0.05,
-      min_bandwidth = NA,
-      max_bandwidth = NA,
-      moment_power = 1,
-      BIC_factor = 2.0,
-      component_eliminate = 0.01,
-      peak_estim_method = "moment",
-      shape_model = "bi-Gaussian"
-    )
+    if(!skip){
+      actual <- extract_features(
+        filenames,
+        cluster = cluster,
+        min_pres = min_pres,
+        min_run = min_run,
+        mz_tol = tol,
+        intensity_weighted = intensity_weighted,
+        sd_cut = sd_cut,
+        sigma_ratio_lim = sigma_ratio_lim,
+        baseline_correct = 0,
+        baseline_correct_noise_percentile = 0.05,
+        min_bandwidth = NA,
+        max_bandwidth = NA,
+        moment_power = 1,
+        BIC_factor = 2.0,
+        component_eliminate = 0.01,
+        peak_estim_method = "moment",
+        shape_model = "bi-Gaussian"
+      )
 
-    expected_filenames <- lapply(files, function(x) {
-      file.path(testdata, "extracted", paste0(x, ".parquet"))
-    })
-    
-    expected <- lapply(expected_filenames, arrow::read_parquet)
-    expected <- lapply(expected, as.data.frame)
+      expected_filenames <- lapply(files, function(x) {
+        file.path(testdata, "extracted", paste0(x, ".parquet"))
+      })
 
-    actual <- unique(actual)
-    expected <- unique(expected)
+      expected <- lapply(expected_filenames, arrow::read_parquet)
+      expected <- lapply(expected, as.data.frame)
 
-    keys <- c("mz", "pos", "sd1", "sd2")
+      actual <- unique(actual)
+      expected <- unique(expected)
 
-    actual <- lapply(actual, function(x) {
-      as.data.frame(x) |> dplyr::arrange_at(keys)
-    })
-    expected <- lapply(expected, function(x) {
-      as.data.frame(x) |> dplyr::arrange_at(keys)
-    })
+      keys <- c("mz", "pos", "sd1", "sd2")
+
+      actual <- lapply(actual, function(x) {
+        as.data.frame(x) |> dplyr::arrange_at(keys)
+      })
+      expected <- lapply(expected, function(x) {
+        as.data.frame(x) |> dplyr::arrange_at(keys)
+      })
 
 
-    for (i in seq_along(files)) {
-      actual_i <- actual[[i]]
-      expected_i <- expected[[i]]
+      for (i in seq_along(files)) {
+        actual_i <- actual[[i]]
+        expected_i <- expected[[i]]
 
-      report <- dataCompareR::rCompare(actual_i, expected_i, keys = keys, roundDigits = 4, mismatches = 100000)
-      dataCompareR::saveReport(report, reportName = files[[i]], showInViewer = FALSE, HTMLReport = FALSE, mismatchCount = 10000)
+        report <- dataCompareR::rCompare(actual_i, expected_i, keys = keys, roundDigits = 4, mismatches = 100000)
+        dataCompareR::saveReport(report, reportName = files[[i]], showInViewer = FALSE, HTMLReport = FALSE, mismatchCount = 10000)
 
-      expect_true(nrow(report$rowMatching$inboth) >= 0.9 * nrow(expected_i))
+        expect_true(nrow(report$rowMatching$inboth) >= 0.9 * nrow(expected_i))
 
-      incommon <- as.numeric(rownames(report$rowMatching$inboth))
+        incommon <- as.numeric(rownames(report$rowMatching$inboth))
 
-      subset_actual <- actual_i %>% dplyr::slice(incommon)
-      subset_expected <- expected_i %>% dplyr::slice(incommon)
+        subset_actual <- actual_i %>% dplyr::slice(incommon)
+        subset_expected <- expected_i %>% dplyr::slice(incommon)
 
-      expect_equal(subset_actual$area, subset_expected$area, tolerance = 0.01 * max(subset_expected$area))
+        expect_equal(subset_actual$area, subset_expected$area, tolerance = 0.01 * max(subset_expected$area))
+      }
     }
   },
   patrick::cases(
@@ -91,7 +93,18 @@ patrick::with_parameters_test_that(
       min_run = 12,
       intensity_weighted = FALSE,
       sd_cut = c(0.01, 500),
-      sigma_ratio_lim = c(0.01, 100)
+      sigma_ratio_lim = c(0.01, 100),
+      skip = FALSE
+    ),
+    qc_no_dil_milliq = list(
+      files = c("8_qc_no_dil_milliq", "21_qc_no_dil_milliq", "29_qc_no_dil_milliq"),
+      tol = 1e-05,
+      min_pres = 0.5,
+      min_run = 12,
+      intensity_weighted = FALSE,
+      sd_cut = c(0.01, 500),
+      sigma_ratio_lim = c(0.01, 100),
+      skip = TRUE
     )
   )
 )
