ea980c09e00351b9b2cfe8121765a7e2ac805cc6
hechth <helge.hecht@recetox.muni.cz>
2022-08-18 17:46:24 +0200

    Added default values for parameters

--- R/compute_clusters.R
+++ R/compute_clusters.R
@@ -19,10 +19,10 @@
 #'   \item mz_tol_relative - float - Newly determined relative mz tolerance.
 #'}
 compute_clusters <- function(feature_tables,
-                             mz_tol_relative,
+                             mz_tol_relative = NA,
                              mz_tol_absolute,
                              mz_max_diff,
-                             rt_tol_relative,
+                             rt_tol_relative = NA,
                              do.plot = FALSE) {
   number_of_samples <- length(feature_tables)
   all <- concatenate_feature_tables(feature_tables, "rt")
