22fc53b042eb630a73d70ff0bd12a1cc588e9240
hechth <helge.hecht@recetox.muni.cz>
2022-08-19 15:48:04 +0200

    Added skipping on ci for one test case

--- tests/testthat/test-hybrid.R
+++ tests/testthat/test-hybrid.R
@@ -1,4 +1,6 @@
 patrick::with_parameters_test_that("basic hybrid test", {
+  if(ci_skip == TRUE) skip_on_ci()
+
   testdata <- file.path("..", "testdata")
 
   test_files <- sapply(files, function(x) {
@@ -18,9 +20,11 @@ patrick::with_parameters_test_that("basic hybrid test", {
   expect_equal(actual$recovered_feature_sample_table, expected)
 }, patrick::cases(
   mbr = list(
-    files = c("mbr_test0.mzml", "mbr_test1.mzml", "mbr_test2.mzml")
+    files = c("mbr_test0.mzml", "mbr_test1.mzml", "mbr_test2.mzml"),
+    ci_skip = TRUE
   ),
   RCX_shortened = list(
-    files = c("RCX_06_shortened.mzML", "RCX_07_shortened.mzML", "RCX_08_shortened.mzML")
+    files = c("RCX_06_shortened.mzML", "RCX_07_shortened.mzML", "RCX_08_shortened.mzML"),
+    ci_skip = FALSE
   )
 ))
