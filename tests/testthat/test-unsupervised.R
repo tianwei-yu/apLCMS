72a544472a3f549fc4414fcf20801e4d4d7a7d1b
hechth <helge.hecht@recetox.muni.cz>
2022-08-19 14:58:35 +0200

    Updated unsupervised test case

--- tests/testthat/test-unsupervised.R
+++ tests/testthat/test-unsupervised.R
@@ -1,7 +1,7 @@
 test_that("basic unsupervised test", {
-  test_files <- c('../testdata/mbr_test0.mzml',
-                  '../testdata/mbr_test1.mzml',
-                  '../testdata/mbr_test2.mzml')
+  test_files <- c('../testdata/input/mbr_test0.mzml',
+                  '../testdata/input/mbr_test1.mzml',
+                  '../testdata/input/mbr_test2.mzml')
   
   expected <- arrow::read_parquet('../testdata/unsupervised_recovered_feature_sample_table.parquet')
   
