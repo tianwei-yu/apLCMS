21221f2b6a44b16f4c1cd7ae84ec2afe21fd9b04
Zargham Ahmad <zargham.ahmad12@gmail.com>
2022-08-26 18:31:01 +0200

    updated benchmark-extract_features test

--- tests/testthat/test-benchmark-extract_features.R
+++ tests/testthat/test-benchmark-extract_features.R
@@ -1,7 +1,7 @@
 patrick::with_parameters_test_that(
   "test benchmark",
   {
-    if(skip){
+    if (skip) {
       skip("Disabled")
     }
 
@@ -13,17 +13,7 @@ patrick::with_parameters_test_that(
       file.path(testdata, "input", paste0(x, ".mzML"))
     })
 
-    # CRAN limits the number of cores available to packages to 2
-    # source https://stackoverflow.com/questions/50571325/r-cran-check-fail-when-using-parallel-functions#50571533
-    chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
-
-    if (nzchar(chk) && chk == "TRUE") {
-      # use 2 cores in CRAN/Travis/AppVeyor
-      cluster <- 2L
-    } else {
-      # use all cores in devtools::test()
-      cluster <- parallel::detectCores()
-    }
+    cluster <- get_num_workers()
 
     if (!is(cluster, "cluster")) {
       cluster <- parallel::makeCluster(cluster)
@@ -32,25 +22,25 @@ patrick::with_parameters_test_that(
 
     res <- microbenchmark::microbenchmark(
       extract_feature = {
-          actual <- extract_features(
-            filenames,
-            cluster = cluster,
-            min_pres = min_pres,
-            min_run = min_run,
-            mz_tol = tol,
-            intensity_weighted = intensity_weighted,
-            sd_cut = sd_cut,
-            sigma_ratio_lim = sigma_ratio_lim,
-            baseline_correct = 0,
-            baseline_correct_noise_percentile = 0.05,
-            min_bandwidth = NA,
-            max_bandwidth = NA,
-            moment_power = 1,
-            BIC_factor = 2.0,
-            component_eliminate = 0.01,
-            peak_estim_method = "moment",
-            shape_model = "bi-Gaussian"
-          )
+        actual <- extract_features(
+          cluster = cluster,
+          filenames,
+          min_presence = min_presence,
+          min_elution_length = min_elution_length,
+          mz_tol = mz_tol,
+          baseline_correct = 0,
+          baseline_correct_noise_percentile = 0.05,
+          intensity_weighted = intensity_weighted,
+          min_bandwidth = NA,
+          max_bandwidth = NA,
+          sd_cut = sd_cut,
+          sigma_ratio_lim = sigma_ratio_lim,
+          shape_model = "bi-Gaussian",
+          peak_estim_method = "moment",
+          component_eliminate = 0.01,
+          moment_power = 1,
+          BIC_factor = 2.0
+        )
       },
       times = 10L
     )
@@ -88,9 +78,9 @@ patrick::with_parameters_test_that(
   patrick::cases(
     RCX_shortened = list(
       filename = c("RCX_06_shortened", "RCX_07_shortened", "RCX_08_shortened"),
-      tol = 1e-05,
-      min_pres = 0.5,
-      min_run = 12,
+      mz_tol = 1e-05,
+      min_presence = 0.5,
+      min_elution_length = 12,
       intensity_weighted = FALSE,
       sd_cut = c(0.01, 500),
       sigma_ratio_lim = c(0.01, 100),
