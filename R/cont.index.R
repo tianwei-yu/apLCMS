f8056baaec67ac9c5f4d6155f7da7d30bc910226
wverastegui <wudmir.rojas@recetox.muni.cz>
2022-10-21 13:05:44 +0000

    use tibble, create functions, add coments
    Co-authored-by: Helge Hecht <helge.hecht@recetox.muni.cz>

--- R/cont.index.R
+++ R/cont.index.R
@@ -1,4 +1,10 @@
-# Computes unique groups
+#' @description
+#' Computes unique groups
+#' @param min.count.run filter parameter. add info here
+#' @param min.pres Run filter parameter. The minimum proportion of presence in the time period for a series of signals grouped
+#'  by m/z to be considered a peak.
+#' @param profile The matrix containing m/z, retention time, intensity, and EIC label as columns.
+#' @return unique.grp. add info here
 compute_uniq_grp <- function(profile, min.count.run, min.pres = 0.6) {
   grps <- profile  
   ttt <- table(grps)
@@ -6,6 +12,7 @@ compute_uniq_grp <- function(profile, min.count.run, min.pres = 0.6) {
   unique.grp <- as.numeric(names(ttt))
   return(unique.grp)
 }
+
 #' @description
 #' Computes the smoothed retention times by using The Nadaraya-Watson kernel regression estimate function.
 #' @param min.run Run filter parameter. The minimum length of elution time for a series of signals grouped by m/z to be considered a peak.
@@ -29,74 +36,16 @@ predict_smoothed_rt <- function(min.run = 5, times) {
   return(smooth)
 }
 
-#' Continuity index
-#'
-#' This is an internal function. It uses continuity index (or "run filter") to select putative peaks from EIC.
-#'
-#' @param newprof The matrix containing m/z, retention time, intensity, and EIC label as columns.
+#' @description
+#' Add descrption
+#' @param min.run Run filter parameter. The minimum length of elution time for a series of signals grouped by m/z to be considered a peak.
 #' @param min.pres Run filter parameter. The minimum proportion of presence in the time period for a series of signals grouped
 #'  by m/z to be considered a peak.
-#' @param min.run Run filter parameter. The minimum length of elution time for a series of signals grouped by m/z to be considered a peak.
-#' @return A list is returned.
-#' \itemize{
-#'   \item new.rec - The matrix containing m/z, retention time, intensity, and EIC label as columns after applying the run filter.
-#'   \item height.rec - The vector of peak heights.
-#'   \item time.range.rec - The vector of peak retention time span.
-#'   \item mz.pres.rec - The vector of proportion of non-missing m/z.
-#' }
-#' @export
-#' @examples
-#' cont.index(newprof, min.pres = min.pres, min.run = min.run)
-cont.index <- function(newprof,
-                       min.pres = 0.6,
-                       min.run = 5) {
-  
-  
-  # newprof[,1] : mz
-  # newprof[,2] : rt
-  # newprof[,3] : intensi
-  # newprof[,4] : EIC labels (Extracted Ions Chromatograms)
-
-  # ordering retention time values
-  labels <- newprof[,2]
-  times <- unique(labels)
-  times <- times[order(times)]  
-  #time.points <- length(times)
- 
-  for (i in 1:length(times)) labels[which(newprof[,2] == times[i])] <- i #now labels is the index of time points
-  newprof[,2] <- labels  
-  
-  # set lower bounds of elution time
-  min.count.run <- min.run * length(times) / (max(times) - min(times))
-  min.run <- round(min.count.run)
-
-  # computes unique groups
-  uniq.grp <- compute_uniq_grp(newprof[, 4], min.count.run)
-
-  # rearrange profile data
-  newprof <- newprof[newprof[, 4] %in% uniq.grp, ] 
-  newprof <- newprof[order(newprof[, 4], newprof[, 1]), ] 
-
-  # computes break points
-  breaks <- compute_breaks_3(newprof[, 4])
-
-  # init counters for loop
-  new.rec <- newprof * 0
-  rec.pointer <- 1
-  curr.label <- 1
-  height.rec <- mz.pres.rec <- time.range.rec <- rep(0, length(breaks))
-  timeline <- rep(0, length(times))
-
-  for (m in 2:length(breaks))
-  {
-    this.prof <- newprof[(breaks[m - 1] + 1):breaks[m], ]
-    this.prof <- this.prof[order(this.prof[, 2]), ]
-    
-    # init loop variables
-    this.mass <- this.prof[, 1]
-    this.times <- this.prof[, 2]
-    this.intensi <- this.prof[, 3]
-    this.grp <- this.prof[1, 4]
+#' @param timeline. 'add info here'
+#' @param this.times. 'add info here'
+#' @param times. Retention times vector.
+#' @return to.keep. 'add info here'
+label_val_keep <- function(min.run, timeline, min.pres, this.times, times) {
     this.timeline <- timeline
     this.timeline[this.times] <- 1
     to.keep <- this.times * 0
@@ -121,18 +70,86 @@ cont.index <- function(newprof,
       measured.points <- measured.points * good.points
       to.keep[which(this.times %in% which(measured.points == 1))] <- 1
     }
+    return(to.keep)
+}
+#' Continuity index
+#'
+#' This is an internal function. It uses continuity index (or "run filter") to select putative peaks from EIC.
+#'
+#' @param newprof The matrix containing m/z, retention time, intensity, and EIC label as columns.
+#' @param min.pres Run filter parameter. The minimum proportion of presence in the time period for a series of signals grouped
+#'  by m/z to be considered a peak.
+#' @param min.run Run filter parameter. The minimum length of elution time for a series of signals grouped by m/z to be considered a peak.
+#' @return A list is returned.
+#' \itemize{
+#'   \item new.rec - The matrix containing m/z, retention time, intensity, and EIC label as columns after applying the run filter.
+#'   \item height.rec - The vector of peak heights.
+#'   \item time.range.rec - The vector of peak retention time span.
+#'   \item mz.pres.rec - The vector of proportion of non-missing m/z.
+#' }
+#' @export
+#' @examples
+#' cont.index(newprof, min.pres = min.pres, min.run = min.run)
+cont.index <- function(newprof,
+                       min.pres = 0.6,
+                       min.run = 5) {
+  
+  newprof <- tibble::tibble(mz = newprof[,1], rt = newprof[,2], intensi = newprof[,3], grps = newprof[,4])
+
+  # ordering retention time values
+  labels <- newprof$rt
+  times <- unique(labels)
+  times <- times[order(times)]  
+  #time.points <- length(times)
+ 
+  for (i in 1:length(times)) labels[which(newprof$rt == times[i])] <- i #now labels is the index of time points
+  newprof$rt <- labels  
+  
+  # set lower bounds of elution time
+  min.count.run <- min.run * length(times) / (max(times) - min(times))
+  min.run <- round(min.count.run)
+
+  # computes unique groups
+  uniq.grp <- compute_uniq_grp(newprof$grps, min.count.run)
+
+  # rearrange profile newprof
+  newprof <- newprof[newprof$grps %in% uniq.grp, ] # dplyr filter
+  newprof <- newprof[order(newprof$grps, newprof$mz), ] # dplyr arrange_at
+
+  # computes break points
+  breaks <- compute_breaks_3(newprof$grps)
+
+  # init counters for loop
+  new.rec <- newprof * 0
+  rec.pointer <- 1
+  curr.label <- 1
+  height.rec <- mz.pres.rec <- time.range.rec <- rep(0, length(breaks))
+  timeline <- rep(0, length(times))
+
+  for (m in 2:length(breaks))
+  {
+    this.prof <- dplyr::slice(newprof, (breaks[m - 1] + 1):breaks[m]) |> dplyr::arrange_at("rt")
+
+    to.keep <- label_val_keep(
+      min.run,
+      timeline,
+      min.pres,
+      this.prof$rt,
+      times
+    )
     
     # operation over filtered indices 
     if (sum(to.keep) > 0) {
       this.sel <- which(to.keep == 1)
-      this.new <- cbind(this.mass[this.sel], this.times[this.sel], this.intensi[this.sel], rep(this.grp, length(this.sel)))
-      r.new <- nrow(this.new)
-      height.rec[curr.label] <- max(this.intensi[this.sel])
-      time.range.rec[curr.label] <- times[max(this.times[this.sel])] - times[min(this.times[this.sel])]
-      mz.pres.rec[curr.label] <- length(this.sel) / (max(this.times[this.sel]) - min(this.times[this.sel]) + 1)
-      curr.label <- curr.label + 1
+      this.new <- dplyr::slice(this.prof, this.sel)
+      r.new <- nrow(this.new) 
 
+      height.rec[curr.label] <- max(this.new$intensi)
+      time.range.rec[curr.label] <- times[max(this.new$rt)] - times[min(this.new$rt)]
+      mz.pres.rec[curr.label] <- length(this.sel) / (max(this.new$rt) - min(this.new$rt) + 1)
       new.rec[rec.pointer:(rec.pointer + r.new - 1), ] <- this.new
+
+      curr.label <- curr.label + 1
       rec.pointer <- rec.pointer + r.new
     }
   }
