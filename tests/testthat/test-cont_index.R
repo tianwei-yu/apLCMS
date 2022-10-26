7db4c2da50fcdd8a840e78919dd945b77701afcd
wverastegui <wudmir@gmail.com>
2022-10-26 09:31:40 +0000

    Refact test-cont.index.R
    Co-authored-by: Helge Hecht <helge.hecht@recetox.muni.cz>

--- tests/testthat/test-cont_index.R
+++ tests/testthat/test-cont_index.R
@@ -5,8 +5,8 @@ patrick::with_parameters_test_that(
 
     testdata <- file.path("..", "testdata")
     input_path <- file.path(testdata, "input", filename)
+
     input_data <- as.matrix(arrow::read_parquet(input_path) ) 
-    
     actual <- cont.index(input_data, min.pres, min.run)
 
     actual <- tibble::tibble(
@@ -16,14 +16,15 @@ patrick::with_parameters_test_that(
       group_number = actual$new.rec[, 4]
       )
 
-    expected_path <- file.path(testdata, "filtered", "output_cont.parquet")
+    expected_path <- file.path(testdata, "filtered", "cont_index", paste0(.test_name, "_cont_index.parquet"))
     expected <- arrow::read_parquet(expected_path) 
   
     expect_equal(actual, expected)
+    
   },
   patrick::cases(
-    input_cont = list(
-      filename = c("input_cont.parquet"),
+    mbr_test0 = list(
+      filename = c("mbr_test0.parquet"),
       min.pres = 0.5,
       min.run = 12,
       ci_skip = FALSE
