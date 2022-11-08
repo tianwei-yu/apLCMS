03060d6e27e3c207affa0e93c147fb8ac621c903
Matej Trojak <trojak@mail.muni.cz>
2022-11-08 10:35:12 +0100

    Additional test cases for run_filter

--- tests/testthat/test-run_filter.R
+++ tests/testthat/test-run_filter.R
@@ -4,9 +4,9 @@ patrick::with_parameters_test_that(
     if(ci_skip == TRUE) skip_on_ci()
 
     testdata <- file.path("..", "testdata")
-    input_path <- file.path(testdata, "filtered", "run_filter", filename)
+    input_path <- file.path(testdata, "filtered", "run_filter", paste0(filename, ".parquet"))
 
-    input_data <- as.matrix(arrow::read_parquet(input_path) ) 
+    input_data <- as.matrix(arrow::read_parquet(input_path))
     actual <- run_filter(input_data, min_pres, min_run)
 
     actual <- tibble::tibble(
@@ -16,17 +16,35 @@ patrick::with_parameters_test_that(
       group_number = actual$new_rec[, 4]
       )
 
-    expected_path <- file.path(testdata, "filtered", "run_filter", paste0(.test_name, "_run_filter.parquet"))
+    expected_path <- file.path(testdata, "filtered", "run_filter", paste0(filename, "_run_filter.parquet"))
     expected <- arrow::read_parquet(expected_path) 
-  
+
     expect_equal(actual, expected)
     
   },
   patrick::cases(
     mbr_test0 = list(
-      filename = c("mbr_test0.parquet"),
+      filename = "mbr_test0",
       min_pres = 0.5,
       min_run = 12,
+      ci_skip = FALSE
+    ),
+    RCX_06_shortened = list(
+      filename = "RCX_06_shortened",
+      min_pres = 0.7,
+      min_run = 4,
+      ci_skip = FALSE
+    ),
+    RCX_07_shortened = list(
+      filename = "RCX_07_shortened",
+      min_pres = 0.7,
+      min_run = 4,
+      ci_skip = FALSE
+    ),
+    RCX_08_shortened = list(
+      filename = "RCX_08_shortened",
+      min_pres = 0.7,
+      min_run = 4,
       ci_skip = FALSE
     )
   )
